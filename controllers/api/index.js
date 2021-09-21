b6d6aaad8dc1bc89982b2dee403865d8ec9b0b0a
friel2021 <friel2009@gmail.com>
2021-09-21 09:25:41 -0600

    updated files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes.js");
 const postRoutes = require("./post-routes");
 const commentRoutes = require("./comment-routes");
-router.use("/user", userRoutes);
-router.use("/post", postRoutes);
-router.use("/comment", commentRoutes);
+router.use("/User", userRoutes);
+router.use("/Post", postRoutes);
+router.use("/Comment", commentRoutes);
 
 module.exports = router;
